c795c35c07b701e44f49ef2f35a27bdf1a14b69e
hackerprank01 <prathameshpgiot222@gst.sies.edu.in>
2025-03-28 23:09:37 +0530

    Initial Commit

---
+++ app/src/citizen/java/com/rescuereach/citizen/CitizenMainActivity.java
@@ -0,0 +1,16 @@
+package com.rescuereach.citizen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rescuereach.R;
+
+public class CitizenMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_citizen_main);
+    }
+}
---
+++ app/src/responder/java/com/rescuereach/responder/ResponderMainActivity.java
@@ -0,0 +1,16 @@
+package com.rescuereach.responder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rescuereach.R;
+
+public class ResponderMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_responder_main);
+    }
+}
